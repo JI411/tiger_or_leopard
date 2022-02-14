26e59b8cde4bd7dba28e58aa5365df6bf718e833
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-02-14 15:43:51 +0700

    add uploading from zip

--- src/app.py
+++ src/app.py
@@ -3,30 +3,37 @@ from PIL import Image
 import io
 
 from const import DATA_DIR, SRC_DIR, WEIGHTS_DIR
+from zipfile import ZipFile
 from src.model import ModelOnnx
 
 
 @st.cache(allow_output_mutation=True)
-def model_load():
+def load_resnet():
     return ModelOnnx(weights=str(WEIGHTS_DIR / 'resnet-18.onnx'))
 
+@st.cache(allow_output_mutation=True)
+def load_efficientnet():
+    return ModelOnnx(weights=str(WEIGHTS_DIR / 'efficientnet-b0.onnx'))
+
+def get_model(model_type):
+    resnet = load_resnet()
+    efficientnet = load_efficientnet()
+    return resnet if model_type == 'resnet' else efficientnet
 
 def app():
 
     st.set_page_config(
-        page_title="Demo"
+        page_title="Tiger or leopard"
     )
 
     with open(SRC_DIR / 'st_form_wo_border.css') as form_style_file:
         st.markdown(f'<style>{form_style_file.read()}</style>', unsafe_allow_html=True)
 
-    st.title("Tiger or leopard")
     st.sidebar.header("Configuration")
 
     PAGES = {
         'From Image': from_image,
         'From ZIP': from_zip,
-        # 'Gallery': gallery,
 
     }
     page = st.sidebar.selectbox('Page:', options=list(PAGES.keys()), key='PAGE_selection')
@@ -44,32 +51,41 @@ def app():
 
 def from_image():
     st.markdown("Загрузите своё фото")
-    st.write('')
 
-    model = model_load()
+    model_type = st.selectbox(
+        'Select model type',
+        ('ResNet', 'EfficientNet')
+    )
+    model = get_model(model_type)
 
-    image = st.file_uploader('Upload photo here', type=['jpg', 'jpeg', 'png'])
-    if image is not None:
+    uploaded_images = st.file_uploader('Upload photo here', type=['jpg', 'jpeg', 'png'],  accept_multiple_files=True)
+    for image in uploaded_images:
+        name = image.name
         image = image.read()
 
         image = Image.open(io.BytesIO(image))
         label = 'tiger' if model(image) == 1 else 'leopard'
 
-        st.markdown(f'**{label}**')
+        st.markdown(f'<h1 align="center"> {label} </h1>', unsafe_allow_html=True)
+        st.markdown(name)
         st.image(image)
 
 def from_zip():
     st.markdown("Загрузите архив с фото")
-    st.write('')
 
-    model = model_load()
+    model_type = st.selectbox(
+        'Select model type',
+        ('ResNet', 'EfficientNet')
+    )
+    model = get_model(model_type)
 
-    image = st.file_uploader('Upload photo here', type=['jpg', 'jpeg', 'png'])
-    if image is not None:
-        image = image.read()
-
-        image = Image.open(io.BytesIO(image))
-        label = 'tiger' if model(image) == 1 else 'leopard'
-
-        st.markdown(f'**{label}**')
-        st.image(image)
+    uploaded_zip = st.file_uploader('Upload zip here', type=['zip'])
+    if uploaded_zip is not None:
+        with ZipFile(uploaded_zip) as archive:
+            for entry in archive.infolist():
+                with archive.open(entry) as file:
+                    with Image.open(file) as image:
+                        label = 'tiger' if model(image) == 1 else 'leopard'
+                        st.markdown(f'<h1 align="center"> {label} </h1>', unsafe_allow_html=True)
+                        st.markdown(entry.filename)
+                        st.image(image)
